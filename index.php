b0651e6d8b19204dfad92c605e0d8543f3fc7aeb
renanmalato <malatows@gmail.com>
2025-07-06 23:22:28 -0400

    initial commit qrcode linktree

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+<head><meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+    
+    <title>Tip Jar Virtual | Renan Malato</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+
+<body>
+        <!-- Alpha.ly Version 0.1 | Development Version -->
+    <div class="container">
+    <div class="col-xs-12">
+            <div class="text-center" style="padding-top: 30px; padding-bottom: 30px;">
+                <img class="backdrop linktree">
+              <h2 style="color: #ffffff; padding-top: 20px;">#GoodMusicOnly</br>Renan Malato</h2><p>Thank you!🙏🏻✨</p>
+            </div>
+    </div>
+    </div>
+
+
+
+
+    <div class="container">
+    <div class="col-xs-12">
+            <div class="text-center">
+                <div style="padding-bottom: 30px;">
+                   <button onclick="location.href='https://enroll.zellepay.com/qr-codes?data=ewogICJuYW1lIiA6ICJGTE9SSURBIE1VTFRJTUVESUEgTExDIiwKICAidG9rZW4iIDogIm9ybGFuZG9mb3RvZmlsbWVAZ21haWwuY29tIiwKICAiYWN0aW9uIiA6ICJwYXltZW50Igp9'" type="button" class="btn btn-outline-light shake" style="width: 80%; padding-top:20px; padding-bottom:20px; font-weight: 800;"><img src="https://renanmalato.com/linktree/img/3zelle-icon.png">
+                      Tip with Zelle</button>
+                </div>
+                <div style="padding-bottom: 30px;">
+                    <button onclick="location.href='https://www.paypal.com/qrcodes/venmocs/702ccaf5-868d-4c0f-9964-7e1ff5935c09?amount=%C2%A05,00&currency_code=USD&created=1669659393.809937'" type="button" class="btn btn-outline-light" style="width: 80%; padding-top:10px; padding-bottom:10px; font-weight: 600;"><img src="https://renanmalato.com/linktree/img/venmo-icon.png">Tip with Venmo</button>
+                </div>
+                <div style="padding-bottom: 30px;">
+                    <button onclick="location.href='https://www.paypal.com/qrcodes/managed/b6e52967-f8f3-4a32-92da-ba6b66d18016?utm_source=consapp'" type="button" class="btn btn-outline-light" style="width: 80%; padding-top:10px; padding-bottom:10px; font-weight: 600;"><img src="https://renanmalato.com/linktree/img/paypall-icon.png">Tip with PayPal</button>
+                </div>
+                <div style="padding-bottom: 30px;">
+                    <button onclick="location.href='https://instagram.com/renanmalato'" type="button" class="btn btn-outline-light" style="width: 80%; padding-top:10px; padding-bottom:10px; font-weight: 600;"><img src="https://renanmalato.com/linktree/img/2instaprofile-icon.png">&nbsp;&nbsp;&nbsp;  Follow on Instagram</button>
+                </div>
+                <div style="padding-bottom: 30px;">
+                    <button onclick="location.href='https://www.musica.renanmalato.com/'" type="button" class="btn btn-outline-light" style="width: 80%; padding-top:10px; padding-bottom:10px; font-weight: 600;"><img src="https://renanmalato.com/linktree/img/website-icon.png">&nbsp;&nbsp;&nbsp;My website</button>
+                </div>
+                              <div style="padding-bottom: 30px;">
+                    <button onclick="location.href='https://www.musica.renanmalato.com/#contactpage'" type="button" class="btn btn-outline-light" style="width: 80%; padding-top:10px; padding-bottom:10px; font-weight: 600;"><img src="https://renanmalato.com/linktree/img/bookeventicon.png">&nbsp;&nbsp;&nbsp; Book a event</button>
+                </div>
+            </div>
+    </div>
+    </div>
+
+        <div class="text-center">
+            <a href="https://musica.renanmalato.com" style="color: #34312f;" target="_blank">Renan Malato #GoodMusicOnly</a>
+        </div>
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <!-- You're welcome to edit, reproduce and change this template as long as original contributation stays present on the website at all times.  -->
+</body>
+
+</html>
